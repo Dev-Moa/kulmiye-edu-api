80ae9fab37b7c4c1df71250ab873fa3528d7967b
Dev-Moa <codewithmoha@gmail.com>
2024-06-03 18:59:44 +0300

    cors

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = (
     "http://localhost:5173",
     "http://localhost:5174",
+    "https://kulmiye-ui.up.railway.app"
 )
 
 # Application definition
